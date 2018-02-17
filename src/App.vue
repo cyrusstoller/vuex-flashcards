ed3f2f11d6f45fcf5157f8a9f4199066d45cec42
Cyrus Stoller <cyrus.stoller@gmail.com>
2018-02-17 12:01:16 -0800

    Aesthetic flourish

--- src/App.vue
+++ src/App.vue
@@ -40,3 +40,18 @@ export default {
   },
 }
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#score {
+  font-size: 24px;
+}
+</style>
--- src/components/Flashcard.vue
+++ src/components/Flashcard.vue
@@ -1,19 +1,22 @@
 <template>
-  <div class="flashcard" @click="flipCard">
-    <div v-show="!isToggle">
+  <div @click="flipCard">
+    <div v-bind:style="{backgroundColor: colorFront, color: colorTextFront}" v-show="!isToggle" class="animated flipInX flashcard">
       <div class="card-content center">
-        <p>{{ front }}</p>
+        <p v-bind:style="{fontSize: textSizeFront,fontWeight: 'bold'}">{{front}}</p>
       </div>
+      <div class="card-footer">{{footerFront}}</div>
     </div>
-    <div v-show="isToggle">
+    <div v-bind:style="{backgroundColor: colorBack, color: colorTextBack}" v-show="isToggle" class="animated flipInX flashcard">
       <div class="card-content center">
-        <p>{{ back }}</p>
+        <p v-bind:style="{fontSize: textSizeBack, fontWeight: 'bold'}">{{back}}</p>
       </div>
+      <div class="card-footer">{{footerBack}}</div>
     </div>
   </div>
 </template>
 
 <script>
+// Inspired by https://github.com/cuduy197/vue-flashcard
 export default {
   props: {
     front: {
@@ -23,6 +26,38 @@ export default {
     back: {
       type: String,
       default: 'default back',
+    },
+    textSizeFront: {
+      type: String,
+      default: '2em'
+    },
+    textSizeBack: {
+      type: String,
+      default: '2em'
+    },
+    colorTextFront: {
+      type: String,
+      default: 'black'
+    },
+    colorTextBack: {
+      type: String,
+      default: 'white'
+    },
+    colorFront: {
+      type: String,
+      default: 'white'
+    },
+    colorBack: {
+      type: String,
+      default: '#2ecc71'
+    },
+    footerFront: {
+      type: String,
+      default: 'Click to show Back'
+    },
+    footerBack: {
+      type: String,
+      default: 'Click to show Front'
     },
   },
   computed: {
@@ -38,11 +73,45 @@ export default {
 }
 </script>
 
-<style>
-  .flashcard {
-    border: 1px black;
-    border-style: solid;
-    padding: 10px;
-    margin: 10px;
+<style scoped>
+.center {
+  text-align: center;
+}
+.flashcard {
+  cursor: pointer;
+  border-radius: 10px;
+  margin: 20px;
+  padding: 25px;
+  box-shadow: 0 0px 10px rgba(0, 0, 0, 0.4);
+  text-align: center;
+}
+.animated {
+  animation-duration: 0.75s;
+  animation-fill-mode: both;
+}
+@keyframes flipInX {
+  from {
+    transform: perspective(400px) rotate3d(1, 0, 0, 90deg);
+    animation-timing-function: ease-in;
+    opacity: 0;
   }
+  40% {
+    transform: perspective(400px) rotate3d(1, 0, 0, -10deg);
+    animation-timing-function: ease-in;
+  }
+  60% {
+    transform: perspective(400px) rotate3d(1, 0, 0, 10deg);
+    opacity: 1;
+  }
+  80% {
+    transform: perspective(400px) rotate3d(1, 0, 0, -5deg);
+  }
+  to {
+    transform: perspective(400px);
+  }
+}
+.flipInX {
+  backface-visibility: visible !important;
+  animation-name: flipInX;
+}
 </style>
